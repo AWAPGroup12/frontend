4fd048bc976c9b900c70086932a222d57e755bb5
miskalaiv <miska090@hotmail.com>
2021-11-19 13:22:23 +0200

    MainUI
    MainUI, Orderhistory, router, added

---
+++ woldora/src/components/Orderhistory.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './Orderhistory.module.css'
+
+
+
+export default function Orderhistory() {
+    return (
+        <div className={styles.container}>
+            Order History
+
+            <div className={styles.historydata}> 
+
+            <div> 40 kpl Chicken McNuggets™ + 6 dippiä. 19,10 € </div>
+            <div> Ben & Jerry’s Half Baked 465ml. 8,90 € </div>
+            <div> Large Pepperoni Stuffed Cheesy Crust & Pepsi Max 1,5l 20€ </div>
+
+            </div>
+        </div>
+    )
+}
