4589445e32c1b5d2c26dc3c7785a4348a53878da
NituX <78910113+NituX@users.noreply.github.com>
2021-11-19 13:24:14 +0200

    Order tracking
    Order tracking base.

--- woldora/src/App.js
+++ woldora/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Login from './components/Login';
 import Register from './components/Register';
+import Delivery from './components/Delivery';
 
 function App() {
   return (
     <div className="App">
-
+      <Delivery/>
     </div>
   );
 }
